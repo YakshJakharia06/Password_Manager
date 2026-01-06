7d421345a106839d17fbb44ec4352f56dba60206
YakshJakharia06 <jakhariayaksh@gmail.com>
2026-01-06 21:38:50 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const Navbar = () => {
   return (
     <nav className='bg-slate-800 text-white flex justify-between px-5 py-10 h-15 items-center'>
-      <div className="logo text-2xl font-bold ">
-        Password-Manager
+      <div className=" flex items-center gap-2 logo text-2xl font-bold ">
+       <img src="/logo-pass.svg" alt="" />  Password-Manager
       </div>
       
       <a href="https://github.com/" target='_blank'>
@@ -16,6 +16,5 @@ const Navbar = () => {
     </nav>
   )
 }
-
 
 export default Navbar
